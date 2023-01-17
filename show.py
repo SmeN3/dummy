0315efb8832b6b0404208534d69134090c7f0416
hlothr <mati.zubowicz@gmail.com>
2023-01-17 15:43:06 +0100

    show

--- show.py
+++ show.py
@@ -14,6 +14,17 @@ def Take_input():
     else:
         Output.insert(END, "Wrong directory")
 
+    words = f.read().split()
+    if words:
+        word = words.pop(0)
+        label.config(text=word)
+        root.after(150, Take_input())
+    else:
+        print('All words typed')
+        root.after_cancel(Take_input())
+
+
+
 
 l = Label(text="set the directory for the text file")
 inputtxt = Text(root, height=1,
@@ -27,11 +38,19 @@ Output = Text(root, height=40,
 Display = Button(root, height=2,
                  width=20,
                  text="Show",
-                 command=lambda: Take_input())
+                 command=lambda: Take_input()
+                 )
 
+label = Label(root, text="")
+
+root.after(150, speed_of_text)
+
+
+label.pack()
 l.pack()
 inputtxt.pack()
 Display.pack()
 Output.pack()
 
 mainloop()
+
