63c3ae6fd1cf0cca761ca4e0578ec563836599ae
hlothr <mati.zubowicz@gmail.com>
2023-01-18 04:13:40 +0100

    ready

--- show.py
+++ show.py
@@ -1,31 +1,31 @@
 from tkinter import *
 import os
+from time import sleep
 root = Tk()
-
 root.title("Show")
 
 
-def Take_input():
-    INPUT = inputtxt.get("1.0", "end-1c")
-    print(INPUT)
-    if os.path.isfile(str(INPUT)) and INPUT.endswith('.txt'):
-        f = open(INPUT, 'r')
-        answer = f.read().split()
+def take_input():
+    input_txt = inputtxt.get("1.0", "end-1c")
+    print(input_txt)
+    if os.path.isfile(str(input_txt)) and input_txt.endswith('.txt'):
+        f = open(input_txt, 'r')
         Output.insert(END, f.read())
     else:
         Output.insert(END, "Wrong directory")
-    return answer
 
 
-def speed_of_text(words):
-
-    if words:
-        word = list(words).pop(0)
+def speed_of_words():
+    words = Output.get("1.0", "end-1c").replace('/n', ' ').split(' ')
+    while words:
+        word = words.pop(0)
+        label_txt.set(word)
+        root.update_idletasks()
         label.config(text=word)
-        root.after(150, speed_of_text(words))
-    else:
-        print('All words typed')
-        root.after_cancel(speed_of_text)
+        sleep(0.15)
+        print(word)
+    print('All words typed')
+
 
 h = Scrollbar(root, orient='horizontal')
 h.pack(
@@ -36,7 +36,7 @@ v = Scrollbar(root)
 v.pack(side=RIGHT, fill=Y)
 
 
-l = Label(text="set the directory for the text file")
+lab = Label(text="set the directory for the text file")
 inputtxt = Text(root, height=1,
                 width=200,
                 bg="light yellow")
@@ -50,23 +50,29 @@ Output = Text(root, height=40,
 Display = Button(root, height=2,
                  width=20,
                  text="Show",
-                 command=lambda: Take_input())
+                 command=lambda: take_input())
 
-label = Label(root, text="", height=10, width= 10,
-              font=('Helvetica bold', 26))
+label_txt = StringVar()
+
+label = Label(root,
+              height=1,
+              width=10,
+              textvariable=label_txt,
+              font=('Helvetica bold', 26),
+              bg="pink"
+              )
 
 Start = Button(root, height=2,
                width=20,
                text='Start',
-               command=lambda: speed_of_text(Take_input),
-
+               command=lambda: speed_of_words(),
                )
 
 
-l.pack()
+lab.pack()
 inputtxt.pack()
 Display.pack()
 Output.pack()
-Start.pack()
 label.pack()
+Start.pack()
 mainloop()
